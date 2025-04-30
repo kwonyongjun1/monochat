5b7e093f2deb5f0f161bf3b4f5db95f823083ba7
kwonyongjun <dydwns18011@gmail.com>
2025-04-30 15:41:41 +0900

    [feat] main 화면 bottomNav 구현

--- src/app/[lng]/_components/MainContainer.tsx
+++ src/app/[lng]/_components/MainContainer.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
-import { CiChat1, CiCircleInfo, CiCirclePlus } from "react-icons/ci";
+import BottomNav from "./BottomNav";
 
 const MainContainer = ({ children }: { children: React.ReactNode }) => {
   const { lng } = useParams();
@@ -16,39 +16,15 @@ const MainContainer = ({ children }: { children: React.ReactNode }) => {
             <Image
               src="/assets/image/logo.png"
               alt="logo"
-              width={75}
-              height={75}
+              width={65}
+              height={65}
             />
           </Link>
         </nav>
       </header>
       {children}
       <footer className="w-full max-w-screen-sm">
-        <nav className="flex justify-center border-t border-t-slate-100 p-1">
-          <ul className="flex w-full justify-between">
-            <li className="flex flex-col items-center w-full icon-fade icon-hover-effect text-sm cursor-pointer">
-              <Link href={`/${lng}`} className="w-full place-items-center ">
-                <CiChat1 size={24} />
-              </Link>
-              대화
-            </li>
-            <li className="flex flex-col items-center w-full icon-fade icon-hover-effect text-sm cursor-pointer">
-              <Link
-                href={`/${lng}/invite`}
-                className="w-full place-items-center"
-              >
-                <CiCirclePlus size={24} />
-              </Link>
-              초대
-            </li>
-            <li className="flex flex-col items-center w-full icon-fade icon-hover-effect text-sm cursor-pointer">
-              <Link href={`/${lng}/info`} className="w-full place-items-center">
-                <CiCircleInfo size={24} />
-              </Link>
-              내정보
-            </li>
-          </ul>
-        </nav>
+        <BottomNav />
       </footer>
     </>
   );
