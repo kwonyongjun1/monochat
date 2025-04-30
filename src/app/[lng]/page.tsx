346c00947904e7ded3a61cdfff3de2fc3b22e059
kwonyongjun <dydwns18011@gmail.com>
2025-04-30 17:09:06 +0900

    [feat] 첫 방문 알림 hook 구현

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,5 +1,32 @@
+"use client";
+
+import { useWelcomeNotify } from "@/hooks";
+import ChatList from "./_components/ChatList";
+
 const Page = () => {
-  return <div>gd</div>;
+  useWelcomeNotify();
+  const testData = [
+    {
+      id: 1,
+      name: "John Doe",
+      lastMessage: "Hello, world!",
+      time: "2021-01-01 12:00:00",
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+      lastMessage: "Hello, world!",
+      time: "2021-01-01 12:00:00",
+    },
+  ];
+
+  return (
+    <main className="flex size-full flex-col overflow-auto animate-duration-150">
+      <article className="flex min-h-full flex-col">
+        <ChatList chatList={testData} />
+      </article>
+    </main>
+  );
 };
 
 export default Page;
