1c44bbeb7a71c8e871c6474531465eb5ed814276
kwonyongjun <dydwns18011@gmail.com>
2025-05-20 14:42:45 +0900

    [feat] 채팅 말풍선 컴포넌트 구현

---
+++ src/app/[lng]/chatting/[id]/_components/ChatBubble.tsx
@@ -0,0 +1,48 @@
+import { cn } from "@/utils/cn";
+
+interface ChatBubbleProps {
+  message: string;
+  sender: string;
+  time: string;
+  isMine: boolean;
+  showSender?: boolean;
+}
+
+const ChatBubble = ({
+  message,
+  sender,
+  time,
+  isMine,
+  showSender = true,
+}: ChatBubbleProps) => {
+  return (
+    <div
+      className={cn("flex items-start gap-2.5", isMine && "flex-row-reverse")}
+    >
+      <div className="flex flex-col w-4/5  leading-1.5 px-4">
+        <div
+          className={cn(
+            "flex items-center",
+            isMine && "flex-row-reverse",
+            !showSender && "hidden"
+          )}
+        >
+          <span className="text-sm font-semibold text-gray-900 ">{sender}</span>
+        </div>
+        <div
+          className={cn(
+            "flex items-center justify-center pt-2 gap-2",
+            isMine && "flex-row-reverse"
+          )}
+        >
+          <p className="text-sm font-normal p-2.5 text-gray-900 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl">
+            {message}
+          </p>
+          <span className="text-sm font-normal text-gray-500">{time}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBubble;
