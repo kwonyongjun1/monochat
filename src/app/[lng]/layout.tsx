95351c6f42c408487ac54d2c7e8ba3778bef7057
kwonyongjun <dydwns18011@gmail.com>
2025-05-26 13:36:47 +0900

    [design] font 적용

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -1,10 +1,13 @@
 import { Metadata } from "next";
 import "@/styles/global.css";
 import "react-toastify/dist/ReactToastify.css";
+import "@radix-ui/themes/styles.css";
 import ToastProvider from "./_components/ToastProvider";
 import GlobalProvider from "./_components/GlobalProvider";
 import MainContainer from "./_components/MainContainer";
 import RightSidePannel from "./_components/RightSidePannel";
+import { cn } from "@/utils/cn";
+import { pretendard } from "@/configs/font";
 
 export const metadata: Metadata = {
   title: "monochat",
@@ -16,11 +19,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html className="size-full" style={{ height: "100dvh" }}>
-      <body className="flex size-full overflow-hidden text-slate-900 justify-center fade-1s">
+    <html
+      className={cn("size-full", pretendard.className)}
+      style={{ height: "100dvh" }}
+    >
+      <body
+        className={cn(
+          "flex size-full overflow-hidden text-slate-900 justify-center fade-1s"
+        )}
+      >
         <GlobalProvider>
           <ToastProvider />
-          <div className="flex-1 max-h-cdvh relative flex h-full w-fit max-w-screen-xl">
+          <div className="flex-1 max-h-cdvh relative flex h-full max-w-screen-xl">
             <div className="relative flex size-full flex-1 flex-col overflow-hidden text-slate-900 border-solid border-t border-t-slate-100">
               <MainContainer>{children}</MainContainer>
             </div>
---
+++ src/configs/font/index.ts
@@ -0,0 +1,22 @@
+import { Noto_Sans_KR, Roboto } from "next/font/google";
+
+export const notoSansKr = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: ["100", "400", "700", "900"],
+  variable: "--notoSansKr",
+});
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "400", "700"],
+  variable: "--roboto",
+});
+
+import localFont from "next/font/local";
+
+export const pretendard = localFont({
+  src: "./PretendardVariable.ttf",
+  display: "swap",
+  weight: "100 900",
+  variable: "--font-pretendard",
+});
